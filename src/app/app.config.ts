c6491117aa1f54d6bcd169956e5583214654759e
Juan Espeche <juanleonidas.espeche.contractor@gdevtools.com>
2025-06-04 13:30:43 -0300

    structure improved and app works

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
@@ -14,9 +10,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimations(),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()),
-    provideStorage(() => getStorage())
   ]
 };
