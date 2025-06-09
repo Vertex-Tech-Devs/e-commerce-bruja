2e63c301277fc2b8594fa70f9f6d4097a3f83a1e
Lihue Leiva <leivalihue@gmail.com>
2025-06-09 13:38:26 -0300

    fix branch

--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -2,24 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-<<<<<<< HEAD
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
-=======
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <div class="dashboard">
-      Dashboard
-    </div>
-  `,
-  styles: [`
-    .dashboard {
-      padding: 2rem;
-    }
-  `]
->>>>>>> 0d5a447835de600f72df83d11a7b36c8f10ff561
 })
 export class DashboardComponent {
 
