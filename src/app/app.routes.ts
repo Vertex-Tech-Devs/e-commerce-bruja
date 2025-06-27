515cb7d45d260abfb12bccef2d331febd9d1a21d
Lihue Leiva <leivalihue@gmail.com>
2025-06-27 16:18:00 -0300

    remove comment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,3 @@
-// import { Routes } from '@angular/router';
-
-// export const routes: Routes = [
-//   {
-//     path: '',
-//     redirectTo: 'admin',
-//     pathMatch: 'full'
-//   },
-//   {
-//     path: 'shop',
-//     loadChildren: () => import('./features/shop/shop.routes').then(m => m.SHOP_ROUTES)
-//   },
-//   {
-//     path: 'product',
-//     loadChildren: () => import('./features/shop/product-detail/product.routes').then(m => m.PRODUCT_ROUTES)
-//   },
-//   {
-//     path: 'cart',
-//     loadChildren: () => import('./features/shop/cart/cart.routes').then(m => m.CART_ROUTES)
-//   },
-//   {
-//     path: 'checkout',
-//     loadChildren: () => import('./features/shop/checkout/checkout.routes').then(m => m.CHECKOUT_ROUTES)
-//   },
-//   {
-//     path: 'admin',
-//     loadChildren: () => import('./features/admin/admin.routes').then(m => m.adminRoutes)
-//   }
-// ];
-
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 
