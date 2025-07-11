6c8d2806316ae413022f71d3ae11188863a5daa7
Lihue Leiva <leivalihue@gmail.com>
2025-07-11 02:09:42 -0300

    16 - Fix - hide sidebar when click in item

--- src/app/features/admin/shared/components/sidebar/sidebar.component.ts
+++ src/app/features/admin/shared/components/sidebar/sidebar.component.ts
@@ -1,25 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
+  standalone: true,
   imports: [CommonModule, RouterModule],
 })
 export class SidebarComponent implements OnInit {
 
   isOpen: boolean = false;
+  isMobile: boolean = false;
 
   constructor() { }
+
+  ngOnInit() {
+    this.checkMobileStatus();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkMobileStatus();
+  }
+
+  private checkMobileStatus(): void {
+    this.isMobile = window.innerWidth < 768;
+    if (!this.isMobile && this.isOpen) {
+      this.isOpen = false;
+    }
+  }
 
   toggleSidebar(): void {
     this.isOpen = !this.isOpen;
   }
 
-  ngOnInit() {
+  hideSidebarOnMobile(): void {
+    if (this.isMobile) {
+      this.isOpen = false;
+    }
   }
-
 }
