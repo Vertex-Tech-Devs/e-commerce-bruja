af27bcb21b3a5c5101d6059948d8496bbd34bab5
Lihue Leiva <leivalihue@gmail.com>
2025-07-11 01:27:40 -0300

    15/14 feature - implement loading-spinner in all of the views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { LoadingComponent } from '@features/admin/shared/components/loading/loading.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
