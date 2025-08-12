e2264a78f05d60484ac6418da3a7ed0a6f85abec
Lihue Leiva <leivalihue@gmail.com>
2025-08-11 23:28:08 -0300

    26-feature ...

--- src/app/features/shop/shop.routes.ts
+++ src/app/features/shop/shop.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ShopComponent } from './layout/shop/shop.component';
 import { HomeComponent } from './components/home/home.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
 
 export const SHOP_ROUTES: Routes = [
   {
@@ -11,6 +12,10 @@ export const SHOP_ROUTES: Routes = [
         path: '',
         component: HomeComponent
       },
+      {
+        path: 'products',
+        component: CatalogComponent
+      }
     ]
   },
 ];
